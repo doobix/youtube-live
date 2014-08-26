554964c3b219c29f10bd829ec5b69642120431d7
Wesley Pascual <wesleypascual@gmail.com>
2014-08-25 17:25:30 -0700

    Added input box for users to add new videos to play

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 var express = require('express');
+var partials = require('express-partials');
 var app = express();
-module.exports = app;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(partials());
+app.use(express.static(__dirname + '/public'));
+
+// Root page
+app.get('/', 
+function(req, res) {
+  res.render('index');
+});
+
+app.listen(8080);
+console.log('Server is listening on 8080');
